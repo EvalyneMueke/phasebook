159dc9195cbfb901493b2f54c8e66b7c3c33dda1
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-12-23 14:16:41 +0300

    Added functionality to search form

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function displayData(data){
     }
 }
 getData()
+
+function searchName(){
+    const form =document.getElementById('searchForm')
+    const input =document.getElementById('name').value
+
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault()
+
+        fetch(`http://localhost:3000/messages?name=${input}`)
+        .then(response =>response.json())
+        .then(data => {
+            if (data.length !=0){
+                const resultsContainer =document.getElementById('resultsContainer')
+                resultsContainer.className='card resultcard'
+                for( item of data){
+                    const resultDetails =`
+                    <img src ="${item.image}" alt ="Student image">
+                    <p> ${item.message}</p>
+                    <h4>${item.name}</h4>
+                    `
+                    resultsContainer.innerHTML=resultDetails
+                }
+            }
+            else{
+                const resultsContainer =document.getElementById('resultsContainer')
+                resultsContainer.className='card resultcard'
+                const notFoundMessage =`<h2>Sorry,Student Not Found</h2>` 
+                resultsContainer.innerHTML=notFoundMessage  
+            }
+        })
+    } )
+
+}
+const searchButton =document.getElementById('searchButton')
+
+searchButton.addEventListener("click", searchName)
